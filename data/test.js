874e0b3be3a3603371362431b8528f0e914376dd
Dani Santos <santos.adaniele@gmail.com>
2020-02-21 18:18:00 -0500

    add more styling to dashboard

--- data/test.js
+++ data/test.js
@@ -1,9 +1,9 @@
 const { users } = require("./users");
 
-const currentUserFriends = ["1030", "1031", "1032"];
+const currentUserFriends = ["1007", "1008", "1009"];
 
 const friends = currentUserFriends.map(friendId => {
-  return users.filter(user => user.id !== friendId);
+  return users.find(user => user.id === friendId);
 });
 
-console.log(friends.length);
+console.log(friends);
